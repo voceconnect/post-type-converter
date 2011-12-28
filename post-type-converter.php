f9a14e4fa1653dbdd63f973414ad4c95a0999a0a
Kevin Langley <klangley@voceconnect.com>
2011-12-28 12:31:32 -0500

    Adding to readme and plugin info

--- post-type-converter.php
+++ post-type-converter.php
@@ -1,12 +1,29 @@
 <?php
 /*
 Plugin Name: Post Type Converter
-Plugin URI: http://voceconnect.com
+Plugin URI: https://github.com/voceconnect/post-type-converter
 Description: Gives a qualified user the ability to convert a post from one post_type to another public post_type.
 Author: Kevin Langley
-Version: 1.0
-Author URI: http://voceconnect.com
+Version: 0.1
+Author URI: http://profiles.wordpress.org/users/kevinlangleyjr
+*******************************************************************
+Copyright 2011-2012 Kevin Langley (email : klangley@voceconnect.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************
 */
+
 if(!class_exists('Post_Type_Converter')) {
 
 	class Post_Type_Converter {
