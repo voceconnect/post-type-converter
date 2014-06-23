34d12098cbf7e6af286f3dd3b102431def256841
Kevin Langley <me@kevinlangleyjr.com>
2014-06-23 11:00:45 -0400

    adding build files and build directories to cap deploy script

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,3 +6,18 @@ set :type, 'plugin'
 
 set :svn_repository, "http://plugins.svn.wordpress.org/#{fetch(:application)}/"
 set :svn_deploy_to, "trunk"
+
+## additional files to exclude from committing to svn
+set :build_files, (
+  fetch(:build_files) << %w{
+    *phpunit.xml.dist
+  }
+).flatten
+
+set :build_folders, (
+  fetch(:build_folders) << %w{
+    *bin
+    *tests
+    config
+  }
+).flatten
